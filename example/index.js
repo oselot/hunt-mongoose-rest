6c8ab6ce2e54843c8bb8bf4546ec12b5f5714129
Ostroumov Anatolij <ostroumov095@gmail.com>
2014-07-02 22:49:19 +0400

    https://oselot.atlassian.net/browse/ACR-211

--- example/index.js
+++ example/index.js
@@ -1,119 +1,26 @@
 var hunt = require('hunt'),
   hrw = require('./../index.js'),
   Hunt = hunt({
-    'huntKey':true,
-    'mongoUrl':'mongodb://localhost/hrw_dev'
+    'huntKey': true,
+    'mongoUrl': 'mongodb://localhost/hrw_dev'
   });
 
-Hunt.extendModel('Article', function(core){
-  var ArticleSchema = new core.mongoose.Schema({
-    'name': {type: String, unique: true},
-    'content': String,
-    'author':  { type: core.mongoose.Schema.Types.ObjectId, ref: 'User' }
-  });
-
-  ArticleSchema.index({
-    name: 1
-  });
-//this step is very important - bind mongoose model to current mongo database connection
-// and assign it to collection in mongo database
-
-
-  ArticleSchema.statics.doSmth = function(user, payload, callback){
-    callback(null, {
-      'user':user,
-      'payload':payload
-    });
-  };
-
-  ArticleSchema.methods.doSmth = function(user, payload, callback){
-    callback(null, {
-      'article': this,
-      'user':user,
-      'payload':payload
-    });
-  };
-
-
-  ArticleSchema.statics.canCreate = function(user, callback){
-    if(user){ //only authorized user can create new article
-      callback(null, true, 'author');
-    } else {
-      callback(null, false);
-    }
-  };
-
-  ArticleSchema.statics.listFilter = function(user, callback){
-    if(user) {
-      if(user.root) {
-        callback(null, {}); //root can list all documents!
-      } else {
-        callback(null, {'owner':user._id}); //non root user can see documents, where he/she is an owner
-      }
-    } else {
-      callback(null, false); //non authorized user cannot list anything!
-    }
-  };
-
-  ArticleSchema.methods.canRead = function(user, callback){
-    if(user) {
-      if(user.root) {
-        callback(null, true, ['name','content','owner'], ['owner']); //root can list all documents and all document fields, with populating author
-      } else {
-        callback(null, this.owner === user._id, ['name','content']); //non root user can see documents, where he/she is an owner
-      }
-    } else {
-      callback(null, false); //non authorized user cannot read anything!
-    }
-  };
-
-  ArticleSchema.methods.canUpdate = function(user, callback){
-    if(user) {
-      if(user.root) {
-//root can list all documents and all document fields, with populating author
-        callback(null, true, ['name','content','owner']);
-      } else {
-        callback(null, this.owner === user._id, ['name','content']);
-//non root user can edit `name` and `content` of
-//documents, where he/she is an owner
-      }
-    } else {
-      callback(null, false); //non authorized user cannot edit anything!
-    }
-  };
-
-  ArticleSchema.methods.canDelete = function(user, callback){
-    var document = this;
-    if(user) {
-      if(user.root) {
-        callback(null, true); //root can delete every document
-      } else {
-        callback(null, document.owner === user._id);
-//non root user can delete documents, where he/she is an owner
-      }
-    } else {
-      callback(null, false); //non authorized user cannot edit anything!
-    }
-  };
-
-
-  return core.mongoConnection.model('Article', ArticleSchema);
-});
+Hunt.extendModel('Article', require('./model/article.model.js'));
 
 hrw(Hunt, { 'modelName': 'Article'});
 
-Hunt.once('start', function(evnt){
+Hunt.once('start', function (evnt) {
   Hunt.async.parallel({
-    'userRoot': function(cb){
+    'userRoot': function (cb) {
       Hunt.model.User.create({
-        'root':true,
+        'root': true,
         'apiKey': Hunt.rack()
       }, cb);
     },
-    'userNonRoot': function(cb){
+    'userNonRoot': function (cb) {
       Hunt.model.User.create({
-        'root':false,
-        'name':{
+        'root': false,
+        'name': {
           'familyName': 'Васильев',
           'middleName': 'Алексей',
           'givenName': 'Артёмович'
@@ -121,27 +28,28 @@ Hunt.once('start', function(evnt){
         'apiKey': Hunt.rack()
       }, cb);
     }
-  },function(error, obj){
-    if(error) {
+  }, function (error, obj) {
+    if (error) {
       throw error;
     } else {
       Hunt.model.Article.findOneAndUpdate(
         { 'name': 'Книжка о хрущике' },
         {
           'name': 'Книжка о хрущике',
-          'content':'Мучной хрущик дышит жопой',
-          'owner':obj.userNonRoot._id
+          'content': 'Мучной хрущик дышит жопой',
+          'author': obj.userNonRoot._id
         },
         { 'upsert': true },
-        function(error, articleCreated){
-          if(error){
+        function (error, articleCreated) {
+          if (error) {
             throw error;
           } else {
-            console.log('Access API as ROOT\n http://localhost:3000/api/v1/trophy?huntKey='+obj.userRoot.apiKey);
-            console.log('Access API as LIMITED\n user http://localhost:3000/api/v1/article?huntKey='+obj.userNonRoot.apiKey);
+            console.log('Access API as ROOT\n http://localhost:3000/api/v1/article?huntKey=' + obj.userRoot.apiKey);
+            console.log('Access API as LIMITED\n user http://localhost:3000/api/v1/article?huntKey=' + obj.userNonRoot.apiKey);
             console.log('Article created', articleCreated);
           }
-        });
+        }
+      );
     }
   });
 });
---
+++ example/model/article.model.js
@@ -0,0 +1,94 @@
+module.exports = exports = function (core) {
+  var ArticleSchema = new core.mongoose.Schema({
+    'name': { type: String, unique: true },
+    'content': String,
+    'author': { type: core.mongoose.Schema.Types.ObjectId, ref: 'User' }
+  });
+
+  ArticleSchema.index({
+    'name': 1,
+    'author': 1
+  });
+//this step is very important - bind mongoose model to current mongo database connection
+// and assign it to collection in mongo database
+
+
+  ArticleSchema.statics.doSmth = function (user, payload, callback) {
+    callback(null, {
+      'user': user,
+      'payload': payload
+    });
+  };
+
+  ArticleSchema.methods.doSmth = function (user, payload, callback) {
+    callback(null, {
+      'article': this,
+      'user': user,
+      'payload': payload
+    });
+  };
+
+
+  ArticleSchema.statics.canCreate = function (user, callback) {
+    if (user) { //only authorized user can create new article
+      callback(null, true, 'author');
+    } else {
+      callback(null, false);
+    }
+  };
+
+  ArticleSchema.statics.listFilter = function (user, callback) {
+    if (user) {
+      if (user.root) {
+        callback(null, {}); //root can list all documents!
+      } else {
+        callback(null, {'author': user._id}); //non root user can see documents, where he/she is an owner
+      }
+    } else {
+      callback(null, false); //non authorized user cannot list anything!
+    }
+  };
+
+  ArticleSchema.methods.canRead = function (user, callback) {
+    if (user) {
+      if (user.root) {
+        callback(null, true, ['id', 'name', 'content', 'owner'], ['owner']); //root can list all documents and all document fields, with populating author
+      } else {
+        callback(null, this.owner === user._id, ['id', 'name', 'content']); //non root user can see documents, where he/she is an owner
+      }
+    } else {
+      callback(null, false); //non authorized user cannot read anything!
+    }
+  };
+
+  ArticleSchema.methods.canUpdate = function (user, callback) {
+    if (user) {
+      if (user.root) {
+//root can list all documents and all document fields, with populating author
+        callback(null, true, ['name', 'content', 'owner']);
+      } else {
+        callback(null, this.owner === user._id, ['name', 'content']);
+//non root user can edit `name` and `content` of
+//documents, where he/she is an owner
+      }
+    } else {
+      callback(null, false); //non authorized user cannot edit anything!
+    }
+  };
+
+  ArticleSchema.methods.canDelete = function (user, callback) {
+    var document = this;
+    if (user) {
+      if (user.root) {
+        callback(null, true); //root can delete every document
+      } else {
+        callback(null, document.owner === user._id);
+//non root user can delete documents, where he/she is an owner
+      }
+    } else {
+      callback(null, false); //non authorized user cannot edit anything!
+    }
+  };
+
+  return core.mongoConnection.model('Article', ArticleSchema);
+};
