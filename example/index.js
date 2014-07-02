edd5a3939792414ea69bafaea21143010bd82e87
Ostroumov Anatolij <ostroumov095@gmail.com>
2014-07-03 00:11:52 +0400

    https://oselot.atlassian.net/browse/ACR-211

--- example/index.js
+++ example/index.js
@@ -1,6 +1,7 @@
 var hunt = require('hunt'),
   hrw = require('./../index.js'),
   Hunt = hunt({
+    'disableCsrf': true,
     'huntKey': true,
     'mongoUrl': 'mongodb://localhost/hrw_dev'
   });
@@ -10,56 +11,7 @@ Hunt.extendModel('Article', require('./model/article.model.js'));
 hrw(Hunt, { 'modelName': 'Article'});
 
 Hunt.once('start', function (evnt) {
-  Hunt.async.parallel({
-    'userRoot': function (cb) {
-      Hunt.model.User.findOneAndUpdate({
-        'apiKey': 'i_am_root'
-      }, {
-        'root': true
-      }, {
-        'upsert': true
-      },
-      cb);
-    },
-    'userNonRoot': function (cb) {
-      Hunt.model.User.findOneAndUpdate({
-        'apiKey': 'i_am_prep'
-      }, {
-        'root': false,
-        'name': {
-          'familyName': 'Васильев',
-          'middleName': 'Алексей',
-          'givenName': 'Артёмович'
-        }
-      }, {
-        'upsert': true
-      },
-      cb);
-    }
-  }, function (error, obj) {
-    if (error) {
-      throw error;
-    } else {
-      Hunt.model.Article.findOneAndUpdate(
-        { 'name': 'Книжка о хрущике' },
-        {
-          'name': 'Книжка о хрущике',
-          'content': 'Мучной хрущик дышит жопой',
-          'author': obj.userNonRoot._id
-        },
-        { 'upsert': true },
-        function (error, articleCreated) {
-          if (error) {
-            throw error;
-          } else {
-            console.log('Access API as ROOT\n http://localhost:3000/api/v1/article?huntKey=' + obj.userRoot.apiKey);
-            console.log('Access API as LIMITED\n user http://localhost:3000/api/v1/article?huntKey=' + obj.userNonRoot.apiKey);
-            console.log('Article created', articleCreated);
-          }
-        }
-      );
-    }
-  });
+  require('./lib/populateDatabase.js')(Hunt);
 });
 
 Hunt.startWebServer();
--- test/nobody.test.js
+++ test/nobody.test.js
@@ -1,3 +1,141 @@
-describe('Testing REST api as nobody', function(){
-  it('will be done');
+var hunt = require('hunt'),
+  hrw = require('./../index.js'),
+  model = require('./../example/model/article.model.js'),
+  populateDb = require('./../example/lib/populateDatabase.js'),
+  should = require('should'),
+  Hunt,
+  request = require('request');
+
+describe('Testing REST api as nobody', function () {
+  before(function (done) {
+    Hunt = hunt({
+      'disableCsrf': true,
+      'huntKey': true,
+      'mongoUrl': 'mongodb://localhost/hrw_dev'
+    });
+
+    Hunt.extendModel('Article', model);
+
+    hrw(Hunt, { 'modelName': 'Article'});
+
+    Hunt.once('start', function (evnt) {
+      populateDb(Hunt, done);
+    });
+
+    Hunt.startWebServer();
+  });
+
+  it('returns unauthorized for GET /', function (done) {
+    request({
+        'method': 'GET',
+        'url': 'http://localhost:' + Hunt.config.port + '/api/v1/article',
+        'json': true
+      },
+      function (error, response, body) {
+        if (error) {
+          done(error);
+        } else {
+          response.statusCode.should.be.equal(401);
+          body.status.should.be.equal('Error');
+          body.errors.should.be.an.Array;
+          body.errors.length.should.be.equal(1);
+          body.errors[0].code.should.be.equal(401);
+          body.errors[0].message.should.be.equal('Authorization required!');
+          done();
+        }
+      });
+  });
+
+  it('returns unauthorized for GET /:id',  function (done) {
+    request({
+        'method': 'GET',
+        'url': 'http://localhost:' + Hunt.config.port + '/api/v1/article/53b43aded6202872e0e3371f',
+        'json': true
+      },
+      function (error, response, body) {
+        if (error) {
+          done(error);
+        } else {
+          response.statusCode.should.be.equal(401);
+          body.status.should.be.equal('Error');
+          body.errors.should.be.an.Array;
+          body.errors.length.should.be.equal(1);
+          body.errors[0].code.should.be.equal(401);
+          body.errors[0].message.should.be.equal('Authorization required!');
+          done();
+        }
+      });
+  });
+
+  it('returns unauthorized for POST /',  function (done) {
+    request({
+        'method': 'POST',
+        'url': 'http://localhost:' + Hunt.config.port + '/api/v1/article',
+        'form': {
+          'name': 'Da book',
+          'content': 'some content'
+        },
+        'json': true
+      },
+      function (error, response, body) {
+        if (error) {
+          done(error);
+        } else {
+          response.statusCode.should.be.equal(401);
+          body.status.should.be.equal('Error');
+          body.errors.should.be.an.Array;
+          body.errors.length.should.be.equal(1);
+          body.errors[0].code.should.be.equal(401);
+          body.errors[0].message.should.be.equal('Authorization required!');
+          done();
+        }
+      });
+  });
+
+  it('returns unauthorized for PUT /:id',  function (done) {
+    request({
+        'method': 'PUT',
+        'url': 'http://localhost:' + Hunt.config.port + '/api/v1/article/53b43aded6202872e0e3371f',
+        'form': {
+          'name': 'Da book',
+          'content': 'some content'
+        },
+        'json': true
+      },
+      function (error, response, body) {
+        if (error) {
+          done(error);
+        } else {
+          response.statusCode.should.be.equal(401);
+          body.status.should.be.equal('Error');
+          body.errors.should.be.an.Array;
+          body.errors.length.should.be.equal(1);
+          body.errors[0].code.should.be.equal(401);
+          body.errors[0].message.should.be.equal('Authorization required!');
+          done();
+        }
+      });
+  });
+
+  it('returns unauthorized for DELETE /:id',  function (done) {
+    request({
+        'method': 'DELETE',
+        'url': 'http://localhost:' + Hunt.config.port + '/api/v1/article/53b43aded6202872e0e3371f',
+        'json': true
+      },
+      function (error, response, body) {
+        if (error) {
+          done(error);
+        } else {
+          response.statusCode.should.be.equal(401);
+          body.status.should.be.equal('Error');
+          body.errors.should.be.an.Array;
+          body.errors.length.should.be.equal(1);
+          body.errors[0].code.should.be.equal(401);
+          body.errors[0].message.should.be.equal('Authorization required!');
+          done();
+        }
+      });
+  });
+
 });
