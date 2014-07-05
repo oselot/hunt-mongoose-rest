c402b1753fa4d8f996bd871fba2b623e22bb20d6
Ostroumov Anatolij <ostroumov095@gmail.com>
2014-07-06 00:30:31 +0400

    https://oselot.atlassian.net/browse/ACR-211

--- test/root.test.js
+++ test/root.test.js
@@ -6,7 +6,7 @@ var hunt = require('hunt'),
   Hunt,
   rootKey = 'i_am_root',
   articleId,
-  bookName = 'Da book'+Date.now(),
+  bookName = 'Da book' + Date.now(),
   request = require('request');
 
 
@@ -78,7 +78,7 @@ describe('Testing REST api as root', function () {
           body.data.name.should.be.equal(bookName);
           body.data.content.should.be.equal('some content');
           body.data.id.should.be.a.String;
-          response.headers.location.should.be.equal('/api/v1/article/'+body.data.id);
+          response.headers.location.should.be.equal('/api/v1/article/' + body.data.id);
           articleId = body.data.id;
           done();
         }
@@ -110,7 +110,8 @@ describe('Testing REST api as root', function () {
   it('Updates content for PUT /:id', function (done) {
     request({
         'method': 'PUT',
-        'url': 'http://localhost:' + Hunt.config.port + '/api/v1/article/'+articleId,
+        'url': 'http://localhost:' + Hunt.config.port + '/api/v1/article/' + articleId,
+        'headers': {'huntKey': rootKey},
         'form': {
           'content': 'some new content'
         },
@@ -125,31 +126,103 @@ describe('Testing REST api as root', function () {
           body.data.name.should.be.equal(bookName);
           body.data.content.should.be.equal('some new content');
           body.data.id.should.be.a.equal(articleId);
-          body.data.author.should.be.a.String;
+          body.data.author.id.should.be.a.String;
+          request({
+            'method': 'GET',
+            'url': 'http://localhost:' + Hunt.config.port + '/api/v1/article/' + articleId,
+            'headers': {'huntKey': rootKey},
+            'json': true
+          },
+            function (error, response, body) {
+              if (error) {
+                done(error);
+              } else {
+                response.statusCode.should.be.equal(200);
+                body.status.should.be.equal('Ok');
+                body.data.name.should.be.equal(bookName);
+                body.data.content.should.be.equal('some new content');
+                body.data.id.should.be.a.equal(articleId);
+                body.data.author.id.should.be.a.String;
+                done();
+              }
+            });
           done();
         }
       });
   });
-/*/
-  it('returns unauthorized for DELETE /:id', function (done) {
+
+  it('returns `Method not allowed` for POST /:id',  function (done) {
     request({
-        'method': 'DELETE',
+        'method': 'POST',
         'url': 'http://localhost:' + Hunt.config.port + '/api/v1/article/53b43aded6202872e0e3371f',
+        'headers': {'huntKey': rootKey},
+        'form': {
+          'name': 'Da book',
+          'content': 'some content'
+        },
         'json': true
       },
       function (error, response, body) {
         if (error) {
           done(error);
         } else {
-          response.statusCode.should.be.equal(401);
+          response.statusCode.should.be.equal(405);
           body.status.should.be.equal('Error');
           body.errors.should.be.an.Array;
           body.errors.length.should.be.equal(1);
-          body.errors[0].code.should.be.equal(401);
-          body.errors[0].message.should.be.equal('Authorization required!');
+          body.errors[0].code.should.be.equal(405);
+          body.errors[0].message.should.be.equal('Method not allowed!');
           done();
         }
       });
   });
-//*/
+
+  it('returns `This API endpoint do not exists!` for some stupid requests',  function (done) {
+    request({
+        'method': 'POST',
+        'url': 'http://localhost:' + Hunt.config.port + '/api/v1/article/someStupidApiEndpointThatDoNotExists',
+        'headers': {'huntKey': rootKey},
+        'form': {
+          'name': 'Da book',
+          'content': 'some content'
+        },
+        'json': true
+      },
+      function (error, response, body) {
+        if (error) {
+          done(error);
+        } else {
+          console.log(body);
+          response.statusCode.should.be.equal(404);
+          body.status.should.be.equal('Error');
+          body.errors.should.be.an.Array;
+          body.errors.length.should.be.equal(1);
+          body.errors[0].code.should.be.equal(404);
+          body.errors[0].message.should.be.equal('This API endpoint do not exists!');
+          done();
+        }
+      });
+  });
+  /*/
+   it('returns unauthorized for DELETE /:id', function (done) {
+   request({
+   'method': 'DELETE',
+   'url': 'http://localhost:' + Hunt.config.port + '/api/v1/article/53b43aded6202872e0e3371f',
+   'json': true
+   },
+   function (error, response, body) {
+   if (error) {
+   done(error);
+   } else {
+   response.statusCode.should.be.equal(401);
+   body.status.should.be.equal('Error');
+   body.errors.should.be.an.Array;
+   body.errors.length.should.be.equal(1);
+   body.errors[0].code.should.be.equal(401);
+   body.errors[0].message.should.be.equal('Authorization required!');
+   done();
+   }
+   });
+   });
+   //*/
 });
